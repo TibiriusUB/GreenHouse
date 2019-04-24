d0c6c6b2614bc4f6d957890a7a2334b0db99109d
Owomugisha Birungi <brinnetbirungi@yahoo.com>
2019-04-23 20:04:52 -0400

    added the detail page that displaces the image and details

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 export default function Card(props) {
     return (
-     
         <div className="col-md-4" style={{marginTop:10, position:'relative'}}>
-            <div class="card">
-                <img class="card-img-top img-responsive" src={props.imageUrl} alt="Card cap"/>
-                <div class="card-body">
+            <div className={props.cardStyle}>
+                <img className="card-img-top img-responsive" src={props.imageUrl} alt="Card cap"/>
+                <div className="card-body">
                     <p style={{fontWeight:'600'}} className="card-titles">{props.title}</p>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
-            <a style={{color:'red', position:'absolute', top:10, right:30}}><i class="fas fa-plus-square fa-2x"></i></a>
+            {
+                props.showPlusSign &&
+                <a href="#!" onClick={props.onPlusClick} style={{color:'red', position:'absolute', top:10, right:30}}><i className="fas fa-plus-square fa-2x"></i></a>
+            }
+            
         </div>
-
     )
 }
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -4,12 +4,24 @@ import plants from './../../utils/plants.json';
 
 export default class LandingPage extends Component {
     state = {
-        plants:[]
+        plants: [],
+        showAllCards: true,
+        showOneCard: false,
+        singleCardData: {},
     }
     componentDidMount() {
         // Fetch data from the database
-       const dataFromDatabase = plants;
-        this.setState({plants: dataFromDatabase})
+        const dataFromDatabase = plants;
+        this.setState({ plants: dataFromDatabase })
+    }
+    onPlusClick = card => {
+        console.log('Birungi', card)
+        this.setState({ singleCardData: card }, () => {
+            this.setState({
+                showAllCards: false,
+                showOneCard: true,
+            })
+        })
     }
 
     render() {
@@ -18,17 +30,51 @@ export default class LandingPage extends Component {
                 <div className="container" style={{ paddingHorizontal: 100 }}>
                     <div className="row">
                         {
+                            this.state.showOneCard &&
+                            <div className="col-md-12">
+                                <div className="row">
+                                    <div class="card col-md-8 " style={{marginTop: 10, maxHeight:650}}>
+
+                                        <div className="col-md-8 offset-md-2" style={{ marginTop: 10, position: 'relative' }}>
+                                            <img className="card-img-top img-responsive" src={this.state.singleCardData.image} alt="Card cap" />
+                                            <div className="card-body">
+                                                <p style={{ fontWeight: '600' }} className="card-titles">{this.state.singleCardData.name}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="col-md-4" style={{ marginTop: 10, position: 'relative', marginBottom: 500 }}>
+                                        <div className="card">
+
+                                            <div className="card-body">
+                                                <p style={{ fontWeight: '600' }} className="card-titles">Details</p>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        }
+
+                        {
+                            this.state.showAllCards &&
                             this.state.plants.map((e, i) => {
                                 return (
                                     <Card
+                                        cardStyle="card"
                                         imageUrl={e.image}
                                         title={e.name}
                                         key={i}
+                                        onPlusClick={() => this.onPlusClick(e)}
+                                        showPlusSign={true}
                                     />
                                 )
                             })
                         }
-
                     </div>
                 </div>
             </div>
